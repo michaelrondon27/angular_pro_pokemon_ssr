92efb5ed96498759ffd0301b7efd4ec8af48d8cf
Michael Rondon <mrondon72@gmail.com>
2025-01-27 15:56:05 +0100

    little changes

--- src/server.ts
+++ src/server.ts
@@ -12,11 +12,6 @@ const indexHtml = join(serverDistFolder, 'index.server.html');
 
 const app = express();
 const commonEngine = new CommonEngine();
-
-export async function netlifyCommonEngineHandler(request: Request, context: any): Promise<Response> {
-    return await render(commonEngine)
-}
-
 /**
  * Example Express Rest API endpoints can be defined here.
  * Uncomment and define endpoints as necessary.
